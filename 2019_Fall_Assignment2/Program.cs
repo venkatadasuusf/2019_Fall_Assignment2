4267297ed904ad698d450e332fba34836edc16e3
Syam Godavarthi <syamg@usf.edu>
2019-10-06 18:32:56 -0400

    Assignment_2_test

--- 2019_Fall_Assignment2/Program.cs
+++ 2019_Fall_Assignment2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _2019_Fall_Assignment2
 {
@@ -24,8 +26,8 @@ namespace _2019_Fall_Assignment2
             string word = "cba";
             Console.WriteLine("Time taken to type with one finger = {0}\n", CalculateTime(keyboard, word));
 
-            int[,] image = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
-            int[,] flipAndInvertedImage = FlipAndInvertImage(image);
+            int[,] matrix = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
+            int[,] flipAndInvertedImage = FlipAndInvertImage(matrix);
             Console.WriteLine("The resulting flipped and inverted image is:\n");
             Display2DArray(flipAndInvertedImage);
             Console.Write("\n");
@@ -116,7 +118,29 @@ namespace _2019_Fall_Assignment2
         {
             try
             {
-                // Write your code here
+                //Console.WriteLine("Please enter keyboard letters :");
+                //string K = Console.ReadLine();
+                //Console.WriteLine("Please enter word : ");
+                //string w = Console.ReadLine();
+                //Checking for constriants
+                
+                //initializing count c
+                int c = 0;
+                
+                if(keyboard.Any(char.IsLower) &&  keyboard.Length == 26 && word.Any(char.IsLower) && (word.Length <= 10 || word.Length >= 1))
+                {
+                    //Only one for loop, Hence satisfying the time complexity of O(n)
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        //Checking and adding the sum
+                        c += keyboard.IndexOf(word[i]);
+                    }
+                    Console.WriteLine("Count is : " + c);
+                }
+                else
+                {
+                Console.WriteLine("Please enter valid Input");
+                }
             }
             catch
             {
@@ -126,11 +150,62 @@ namespace _2019_Fall_Assignment2
             return 0;
         }
 
-        public static int[,] FlipAndInvertImage(int[,] A)
+        public static int[,] FlipAndInvertImage(int[,] matrix)
         {
             try
             {
-                // Write your code here
+                int rlen = matrix.GetLength(0);
+                int clen = matrix.GetLength(1);
+
+                //Console.WriteLine(rlen);
+                //Checking for rows then columns.
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        //put a single value
+                        //Console.Write(string.Format("{0} ", matrix[i, j]));
+                        //Console.WriteLine();
+                        //checking for constraints
+                        if((rlen <= 20 || rlen >= 1) && (matrix[i,j] == 0 || matrix[i,j] == 1))
+                        {
+                            int l = 0, r = matrix.GetLength(1) - 1;
+                            var t = matrix[i, l];
+
+                            while (l < r)
+                            {
+                            matrix[i, l] = matrix[i, r];
+                            matrix[i, r] = t;
+                            l++; r--;
+                            }
+                            //Replacing the values
+                            if (matrix[i,j] == 1)
+                            {
+                                matrix[i, j] = 0;
+                            }
+                            else
+                            {
+                                matrix[i, j] = 1;
+                            }
+
+                            //Console.WriteLine(matrix[i, j]);
+
+                            Console.Write(matrix[i, j]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter valid Input");
+                        }
+
+
+                    }
+                    //to print the output in matrix format
+                    Console.Write(Environment.NewLine + Environment.NewLine);
+   
+                }
+                            
+                      
+                                    
             }
             catch
             {
@@ -144,7 +219,59 @@ namespace _2019_Fall_Assignment2
         {
             try
             {
-                // Write your code here
+                int[] start = new int[(intervals.Length) / 2];
+                int[] end = new int[(intervals.Length) / 2];
+
+                //Initializing start and end timings.
+                for (int i = 0; i < (intervals.Length) / 2; i++)
+                {
+                    start[i] = intervals[i, 0];
+                    end[i] = intervals[i, 1];
+
+                }
+
+
+                //Sorting two arrays
+                Array.Sort(start);
+                Array.Sort(end);
+
+                //Dictionary to get minimum no of rooms
+                Dictionary<int, int> minrooms = new Dictionary<int, int>();
+
+                minrooms.Add(1, end[0]);
+
+                int room = 0;
+
+                for (int i = 1; i < start.Length; i++)
+                {
+                    //checking constraint
+                    if (start[i] >= end[i])
+                    {
+                                Console.WriteLine("Invalid Input");
+                    }
+                    else
+                    {
+                        room = 0;
+                        for (int j = 1; j <= (minrooms.Count); j++)
+                        {
+                            if (start[i] > minrooms[j])
+                            {                            
+                                minrooms[j] = end[i];
+                                room = 1;
+                                break;
+                            
+                            }
+                        }
+                    }
+
+                    if (room == 0)
+                    {
+                        minrooms.Add((minrooms.Count + 1), end[i]);
+                    }
+
+                }
+
+                Console.WriteLine(minrooms.Count);
             }
             catch
             {
